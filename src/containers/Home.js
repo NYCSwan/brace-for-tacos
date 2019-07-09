27a72000cdacc2979d5b1610628cb2337c83a97c
nycswan <megan.swanby@gmail.com>
2019-07-09 13:02:41 -0400

    Update home styling.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,13 +4,12 @@ import Search from '../ui/Search'
 import Taco from '../ui/Taco'
 
 function Home() {
-  const [data, setData] = useState({ tacos: [] });
-const [loading, setLoading] = useState(true);
+  const [tacos, setTacos] = useState({ tacos: [] });
+
   return (
     <Outer>
-        <p>Everyone loves tacos! I love to cook and eat. It's fundamental part of who I am. Click on the button to find a recipe.</p>
+        <Header>Everyone loves tacos! I love to cook and eat. It's fundamental part of who I am. Click on the button to find a recipe.</Header>
         <Search />
-        {data.length > 1 && <Taco />}
       </Outer>
   );
 }
@@ -18,5 +17,9 @@ const [loading, setLoading] = useState(true);
 const Outer = styled.div`
   height: 100%;
 `;
-
+const Header = styled.div`
+  font-size: 1rem;
+  line-height: 1.5rem;
+  width: 70%;
+`;
 export default Home;
