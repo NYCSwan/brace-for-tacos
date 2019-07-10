787e449ed7c4ff27b82506204fc78a5334bb6dd9
nycswan <megan.swanby@gmail.com>
2019-07-10 09:29:28 -0400

    Update routes to include taco list. Add app styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import styled from "styled-components";
 import Home from "./containers/Home";
+import TacoList from "./ui/TacoList";
 import Footer from "./ui/Footer";
 import Header from "./ui/Header";
-
+import ThemeContext from "./utils/Context";
 const AppContainer = styled.div`
-  text-align: center;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
 `;
 
-function App() {
+const App = () => {
+  const themeHook = useState("darkblue");
   return (
-    <Router>
-      <Header />
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <Router>
+          <Header />
           <AppContainer>
             <Route exact path="/" component={Home} />
+            <Route path="/tacos" component={TacoList} />
           </AppContainer>
           <Footer />
         </Router>
+      </ThemeContext.Provider>
+    </React.StrictMode>
   );
-}
+};
 
 export default App;
