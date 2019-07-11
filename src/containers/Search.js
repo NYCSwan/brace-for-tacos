e80452d8bbee3424957c6b85fe730a68c1dfa228
nycswan <megan.swanby@gmail.com>
2019-07-11 14:30:24 -0400

    Remove unused imports, rfactor onbutpress- move to home

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,43 +1,12 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import styled from "styled-components";
-import Taco from "../ui/Taco";
-import SearchParams from "../ui/SearchParams";
 import ThemeContext from "../utils/Context";
 
 function Search({ onButtonClick }) {
-  const [taco, setTaco] = useState({});
   const [loading, setLoading] = useState(false);
-  const [tacos, addTacos] = useState([]);
-  const [theme] = useContext(ThemeContext);
-  //
-  // async function onButtonClick() {
-  //   console.log("click");
-  //   if (loading) return;
-  //   setLoading(true);
-  //   const result = await fetch("https://taco-randomizer.herokuapp.com/random/");
-  //   const taco = await result.json();
-  //   setTaco(taco);
-  //   console.log("taco", taco);
-  //   // console.log("tacos", tacos);
-  //   setLoading(false);
-  //
-  //   return taco;
-  // }
-  //
-  // useEffect(() => {
-  //   function updateTacos() {
-  //     const newTacos = tacos;
-  //     // debugger;
-  //     newTacos.push(taco);
-  //     addTacos(newTacos);
-  //   }
-  //   if (taco.hasOwnProperty("mixin")) {
-  //     updateTacos();
-  //   }
-  //   console.log("tacos", tacos);
-  // }, [taco]);
 
-  // <SearchParams />
+  const [theme] = useContext(ThemeContext);
+
   return (
     <div>
       <Button
