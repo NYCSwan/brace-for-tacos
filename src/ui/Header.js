01ad057985baf09ba9c48b372ac3c4511e288750
nycswan <megan.swanby@gmail.com>
2019-07-11 14:34:27 -0400

    Fix router link tag. Style nav items, logo

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,28 +1,46 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../swan_logo.png";
+import Home from "../containers/Home";
+import TacoList from "./TacoList";
 
 function Header() {
   return (
     <Nav>
       <Logo src={logo} alt="logo" />
+      <NavLink to="/" activeClassName="selected">
+        Home{" "}
+      </NavLink>
+      <NavLink to="/tacos" activeClassName="selected">
+        Tacos{" "}
+      </NavLink>
     </Nav>
   );
 }
 
 const Nav = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   background-color: #282c34;
-  min-height: 5vh;
+  min-height: 6vh;
   display: flex;
-  flex-direction: column;
+  flex-direction: row wrap;
   align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
+  justify-content: space-around;
+
+  > a {
+    font-size: calc(8px + 1vmin);
+    text-decoration: none;
+    color: white;
+  }
 }
 `;
 const Logo = styled.img`
-  height: 2vmin;
+  height: 3vmin;
+  width: auto;
   pointer-events: none;
 }
 `;
