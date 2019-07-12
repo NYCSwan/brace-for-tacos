7b3ef5e75d6dc8bfef07cc207e98312e394adf10
nycswan <megan.swanby@gmail.com>
2019-07-12 18:45:34 -0400

    Update header styling. Remove second route. Update taco styling, format list. Add tags to taco display.

--- src/ui/Taco.js
+++ src/ui/Taco.js
@@ -1,42 +1,52 @@
 import React from "react";
 import styled from "styled-components";
-import { mapKeys } from "lodash";
 import ErrorBoundary from "../containers/ErrorBoundary";
+import recipeImage from "./recipe-card.png";
 
-function Taco({ taco, match }) {
-  // const [tacos, addTacos] = useState([]);
-  // const [taco, setTaco] = useState({});
+function Taco({ taco }) {
   return (
     <TacoRecipe>
       <Header>{taco.base_layer.name} Taco</Header>
       <Section>
+        {taco.base_layer.description ? (
+          <Description>{taco.base_layer.description}</Description>
+        ) : null}
+
         <Name>{taco.base_layer.name}</Name>
-        <Recipe>
-          {taco.base_layer.description ? taco.base_layer.description : null}
-        </Recipe>
-      </Section>
-      <Section>
+        {taco.base_layer.instructions
+          ? taco.base_layer.instructions.map(line => {
+              return <Ingredient>{line}</Ingredient>;
+            })
+          : null}
         <Name>{taco.mixin.name}</Name>
-        <Recipe>
-          {taco.mixin.description ? taco.mixin.description : null}
-        </Recipe>
-      </Section>
-      <Section>
+
+        {taco.mixin.description ? (
+          <Recipe>{taco.mixin.description}</Recipe>
+        ) : null}
+
+        {taco.mixin.instructions
+          ? taco.mixin.instructions.map(line => {
+              return <Ingredient>{line}</Ingredient>;
+            })
+          : null}
         <Name>{taco.condiment.name}</Name>
-        <Recipe>
-          {taco.condiment.description ? taco.condiment.description : null}
-        </Recipe>
-      </Section>
-      <Section>
+
+        {taco.condiment.directions ? (
+          <Recipe>{taco.condiment.directions}</Recipe>
+        ) : null}
+
         <Name>{taco.seasoning.name}</Name>
-        <Recipe>
-          {taco.seasoning.description ? taco.seasoning.description : null}
-        </Recipe>
-      </Section>
-      <Section>
+
+        {taco.seasoning.directions ? (
+          <Recipe>{taco.seasoning.directions}</Recipe>
+        ) : null}
+
+        {taco.seasoning.directions ? (
+          <Recipe>{taco.seasoning.directions}</Recipe>
+        ) : null}
         <Name>{taco.shell.name}</Name>
       </Section>
-      
+      {taco && taco.tags.length > 1 ? <Tag>{taco.tags}</Tag> : null}
     </TacoRecipe>
   );
 }
@@ -51,50 +61,72 @@ export default function TacoWithErrorBoundary(props) {
 
 const TacoRecipe = styled.div`
   height: auto;
-  width: 70%;
+  min-height: 350px;
+  width: 45%;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
   overflow: visible;
+  background-color: #efefef;
+  background-image: url(${recipeImage});
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
 const Header = styled.h2`
   color: grey;
-  font-size: 1rem;
-  line-height: 1.4rem;
+  font-size: 1em;
+  line-height: 1.4em;
   text-align: center;
 `;
 
 const Section = styled.ul`
   height: auto;
-  width: 100%;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: flex-start;
-  align-items: center;
+  width: 90%;
+  list-style: none;
+  padding-top: 5%;
+  padding-bottom: 10%;
 `;
 
 const Name = styled.li`
   text-decoration: none;
   color: grey;
-  font-size: 0.8rem;
-  line-height: 1.2rem;
-  text-align: center;
+  font-size: 0.9rem;
+  line-height: 1.3rem;
+  text-align: left;
 `;
 
 const Recipe = styled.li`
   text-decoration: none;
   color: darkgrey;
   font-size: 0.8rem;
-  line-height: 1.4rem;
+  line-height: 1.2rem;
   text-align: left;
 `;
 
-const Tag = styled.li`
+const Description = styled.li`
   text-decoration: none;
+  color: darkgrey;
+  font-size: 0.8rem;
+  line-height: 1rem;
+  text-align: left;
+  width: 80%;
+  padding-bottom: 10px;
+`;
+
+const Ingredient = styled.li`
+  color: darkgrey;
+  font-size: 0.7rem;
+  line-height: 1rem;
+  text-align: left;
+  display: inline;
+`;
+
+const Tag = styled.li`
   color: darkgreen;
   font-size: 0.7rem;
   line-height: 1rem;
   text-align: left;
+  list-style: none;
 `;
